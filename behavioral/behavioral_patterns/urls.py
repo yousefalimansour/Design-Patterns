78309db31caf45d78e0ef850dd42d74e3a6fe2b6
Yousef Ali <you@example.com>
2025-11-29 21:14:42 +0200

    Finished

--- behavioral/behavioral_patterns/urls.py
+++ behavioral/behavioral_patterns/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chain/', include('chain_of_responsibility.urls')),
+    path('command/', include('command.urls')),
 ]
